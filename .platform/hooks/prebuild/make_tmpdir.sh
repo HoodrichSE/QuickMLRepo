c76df16c11e4258e7b54621fa08cf79154185ad5
Spencer Goodrich <goodrichse@gmail.com>
2025-07-19 14:19:35 -0700

    Fixed typo in filename

--- .platform/hooks/prebuild/make_tmpdir.sh
+++ .platform/hooks/prebuild/make_tmpdir.sh
@@ -6,3 +6,4 @@ printf "Creating temporary dir and cache for pip..."
 mkdir -p /var/app/pip_temp && chmod 777 /var/app/pip_temp
 mkdir -p /var/app/pip_cache && chmod 777 /var/app/pip_cache
 #export TMPDIR=/var/app/pip_temp #Could also use /var/tmp
+#python3 -m pip config set global.cache-dir "/var/app/pip_cache"
