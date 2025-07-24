4ff679c38d16d158de73f5a5ddd964eb9a39e17a
Spencer Goodrich <goodrichse@gmail.com>
2025-07-23 18:01:08 -0700

    Resolved dep confusion for pip install

---
+++ .platform/confighooks/prebuild/make_tmpdir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o
+set -e
+
+
+#printf "Creating temporary dir and cache for pip..."
+#mkdir -p /var/app/pip_temp && chmod 777 /var/app/pip_temp
+#mkdir -p /var/app/pip_cache && chmod 777 /var/app/pip_cache
+export TMPDIR=/var/app/pip_temp #Could also use /var/tmp
+printf "$TMPDIR"
+python3.12 -m pip config set global.cache-dir "/var/app/pip_cache"
