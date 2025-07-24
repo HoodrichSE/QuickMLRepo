87751e4fa1ed2c750e78c351efc67236026d71a9
Spencer Goodrich <goodrichse@gmail.com>
2025-07-23 18:44:02 -0700

    Removed redundant cache dirs

--- .platform/hooks/prebuild/02_make_tmpdir.sh
+++ .platform/hooks/prebuild/02_make_tmpdir.sh
@@ -3,9 +3,9 @@ set -o
 set -e
 
 
-printf "Creating temporary dir and cache for pip..."
-mkdir -p /var/app/pip_temp && chmod 777 /var/app/pip_temp
-mkdir -p /var/app/pip_cache && chmod 777 /var/app/pip_cache
-export TMPDIR=/var/app/pip_temp #Could also use /var/tmp
-printf "$TMPDIR"
-python3.12 -m pip config set global.cache-dir "/var/app/pip_cache"
+#printf "Creating temporary dir and cache for pip..."
+#mkdir -p /var/app/pip_temp && chmod 777 /var/app/pip_temp
+#mkdir -p /var/app/pip_cache && chmod 777 /var/app/pip_cache
+#export TMPDIR=/var/tmp
+#printf "$TMPDIR"
+python3.12 -m pip config set global.cache-dir "/var/tmp"
