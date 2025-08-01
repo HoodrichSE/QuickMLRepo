d71754701f3ee5f42dc81ad1b57e849903aaa059
Spencer Goodrich <goodrichse@gmail.com>
2025-07-31 17:08:17 -0700

    Debugging pip install more

---
+++ .platform/hooks/predeploy/02_make_tmpdir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o
+set -e
+
+
+#printf "Creating temporary dir and cache for pip..."
+#mkdir -p /var/app/pip_temp && chmod 777 /var/app/pip_temp
+#mkdir -p /var/app/pip_cache && chmod 777 /var/app/pip_cache
+#export TMPDIR=/var/tmp
+#printf "$TMPDIR"
+python3.12 -m pip config set global.cache-dir "/var/tmp"
+python3.12 -m pip cache dir
